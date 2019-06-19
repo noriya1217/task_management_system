9459c8fe0a6df3fa576bbc1855d8b92415568e16
Noriya Saeki <noriya.saeki@gmail.com>
2019-06-19 11:03:32 +0900

    add: Basic認証追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :basic
+
+  private
+  def basic
+    authenticate_or_request_with_http_basic do |name, password|
+      name == 'hoge' && password == 'piyo'
+    end
+  end
 end
