736b16f505ca30e0b090d1e1cb82b172108b62b8
Noriya Saeki <noriya.saeki@gmail.com>
2019-06-28 14:27:30 +0900

    fix: ログイン画面にpassword_confirmation追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    if user && user.authenticate(params[:session][:password]) && user.authenticate(params[:session][:password_confirmation])
       session[:user_id] = user.id
       redirect_to user_path(user.id), notice: 'ログインしました'
     else
