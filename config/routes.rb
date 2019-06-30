e3c468927df2bcfb2d82f7dd0e897476692225c7
Noriya Saeki <noriya.saeki@gmail.com>
2019-06-30 23:25:24 +0900

    feat: ルーティングにadmin追加、controllerとviewにadminフォルダ作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   root to: 'sessions#new'
   resources :tasks
   resources :users, only:[:new, :create, :show]
+  namespace :admin do
+    resources :users
+  end
   resources :sessions, only:[:new, :create, :destroy]
 end
