f960fd32122d10afd928e74345553b6e110e2b71
Noriya Saeki <noriya.saeki@gmail.com>
2019-07-03 18:20:25 +0900

    fix: admin/user/create失敗時及びtask/update失敗時に一部のデータが表示されてない問題を修正

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -14,11 +14,12 @@ class Admin::UsersController < ApplicationController
   def create
     params[:user][:admin_attributes] = nil if params[:user][:admin_attributes][:user_id] == '無'
     @user = User.new(user_params)
-    # ユーザー新規登録にて管理者権限を'有'の時User.last.id + 1をparams[:user][:admin_attributes][:user_id]に入れてみたが、Admin.newに入れるのに失敗する。
+    # TODO: ユーザー新規登録にて管理者権限を'有'の時User.last.id + 1をparams[:user][:admin_attributes][:user_id]に入れてみたが、Admin.newに入れるのに失敗する。
     # しかし、@user.saveを実行すると対応Adminテーブルにもsaveしたuserのidが入っている。これはreferencesされているから？要調査。
     if @user.save
       redirect_to admin_users_path, notice: 'アカウント作成しました'
     else
+      @user.build_admin
       render 'new'
     end
   end
